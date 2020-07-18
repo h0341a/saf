eeae07d38e65cb396747db33d58989d08b6aea04
ling <qianaiwuyu@gmail.com>
2020-07-18 13:54:52 +0800

    init

---
+++ src/main/java/com/ccjr/saf/SocialAppFrameApplication.java
@@ -0,0 +1,13 @@
+package com.ccjr.saf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialAppFrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialAppFrameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccjr/saf/SocialAppFrameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccjr.saf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialAppFrameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
